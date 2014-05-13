de8f4f3176ef404dc4eee2afeba2d034bd5c2792
Rahul Trikha <rahul.trikha@gmail.com>
2014-05-13 12:36:31 +0930

    Added basic idea addition

---
+++ app/models/idea.js
@@ -0,0 +1,4 @@
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  votes: DS.attr('number')
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,1 +1,23 @@
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+  model: function () {
+    return this.store.find('idea');
+  },
+
+  actions: {
+    submit: function (e) {
+      var self  = this;
+      var store = self.store;
+      var idea  = self.controller.get('idea');
+      if (idea.length < 10) {
+        alert('Please enter atleast 10 characters');
+      } else {
+        var ideaRecord = store.createRecord('idea', { title: idea });
+        ideaRecord.save().then(function() {
+          self.controller.set('idea', '');
+        });
+      }
+
+      return false;
+    }
+  }
+});
