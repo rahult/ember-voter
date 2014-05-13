bcd223ba20ac3d01ca23be83958a39a618d10ea2
Rahul Trikha <rahul.trikha@gmail.com>
2014-05-13 14:49:38 +0930

    Updated grunt file to deploy a production release

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,33 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    'gh-pages': {
-      options: {
-        base: 'dist'
-      },
-      src: ['**']
+    clean: ['dist'],
+    rsync: {
+      deploy: {
+        options: {
+          args: ["--verbose"],
+          exclude: [".DS_Store"],
+          recursive: true,
+          src:  "dist/",
+          dest: "/home/30874/users/.home/domains/ember-voter.rahultrikha.com/html",
+          host: "ember-voter.rahultrikha.com",
+          syncDestIgnoreExcl: true
+        }
+      }
     },
     exec: {
       build: {
-        command: 'ember build production',
+        command: 'ember build --environment=production',
         stdout: false,
         stderr: false
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-gh-pages');
   grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-rsync');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('build',  ['exec:build']);
-  grunt.registerTask('deploy', ['build', 'gh-pages']);
+  grunt.registerTask('build',  ['clean', 'exec:build']);
+  grunt.registerTask('deploy', ['build', 'rsync:deploy']);
 };
